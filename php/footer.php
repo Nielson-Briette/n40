89e519988506f8060b7630b17e6b021e57dce6a9
Briette Nielson <Briette Nielson>
2016-11-16 16:23:52 -0800

    php to server test

---
+++ php/footer.php
@@ -0,0 +1,20 @@
+    <div class="container-fluid">
+        <div class="row">
+            <h4>About</h4>
+            <ul class="list-unstyled">
+                <li><a href="/onepage.html">Intro</a></li>
+                <li><a href="/aboutus.html">About Us</a></li>
+                <li><a href="/photography.html">Photography</a></li>
+                <li><a href="/videography.html">Videography</a></li>
+                <li><a href="/pricing.html">Pricing</a></li>
+            </ul>
+            <h4>Follow</h4>
+            <ul class="list-inline">
+                <li>
+                    <a rel="nofollow" href="https://www.youtube.com/channel/UCXkQroKrmu42OT0uZuR0ySA" title="YouTube"><i class="ion-social-youtube-outline"></i></a>&nbsp;</li>
+                <li><a rel="nofollow" href="https://www.facebook.com/n40photography/?fref=ts" title="Facebook"><i class="ion-social-facebook-outline"></i></a>&nbsp;</li>
+                <li><a rel="nofollow" href="https://www.instagram.com/n40photography/" title="Instgram"><i class="ion-social-instagram-outline"></i></a></li>
+                <li><a rel="nofollow" href="https://vimeo.com/167965947" title="Vimeo"><i class="ion-social-vimeo"></i></a></li>
+            </ul>
+        </div>
+    </div>
---
+++ php/header.php
@@ -0,0 +1,33 @@
+<nav id="topNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+
+            </div>
+            <div class="navbar-collapse collapse" id="bs-navbar">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a class="page-scroll" href="/onepage.html">Home</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/onepage.html#two">About us</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/onepage.html#three">Photography</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/onepage.html#four">Videography</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/onepage.html#five">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
